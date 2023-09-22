1d37e7402a8d615b959f00970d4cb1d4530db826
Gustavo-b017 <gustavo.estudo017@gmail.com>
2023-09-22 20:12:17 -0300

    unindo front-003 e organizando

---
+++ aula_02/aula01.js
@@ -0,0 +1,19 @@
+// Criando um alerta em JavaScript
+alert("olá mundo!");
+/*
+    ****************************************************************************************
+    - Obs: o comando "alert" é uma função que exibe mensagens na tela do usuário.
+    - adicionando
+    - Criando um nojo Projeto
+    **************************************************************************************** 
+*/
+
+// Adicionado uma mensagem na tela
+document.write("olá mundo!");
+
+// add console.log (manda uma mensagem para o programador)
+console.log("Olá meu nome é gustavo");
+
+// Criando uma Variavel
+let mensagem = "ola mundo";
+console.log("mensagem de variavel",mensagem);
