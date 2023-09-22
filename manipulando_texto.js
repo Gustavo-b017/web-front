43562bf1ab4da29880b761b3bc45a55c9b756bfa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:43:12 -0300

    passo a passo e textos

---
+++ manipulando_texto.js
@@ -0,0 +1,7 @@
+let numFloat = 123.543;
+let linguagens = ["java", "c#", "python"];
+
+// .toString - funcao nativa do javascript, converte um numero em string
+///console.log (typeof numFloat.toString());
+//console.log (typeof String(numFloat)); - funcao mais recente com a mesma funcionaliadade
+console.log(linguagens)
