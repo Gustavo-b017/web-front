acfcf2a16304e821fb7bc442eee234d5b81c8611
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 10:44:38 -0300

    decremento e incremento

---
+++ aulas/aula_07/incremento.js
@@ -0,0 +1,40 @@
+let soma = 7;
+
+console.log(soma);
+
+soma++;
+
+console.log(soma);
+
+soma += 2;
+
+console.log(soma);
+
+// para subtrair
+
+let sub = 7;
+
+console.log(sub);
+
+sub--;
+
+console.log(sub);
+
+sub -= 2;
+
+console.log(sub);
+
+// para multiplicar
+
+let mult = 7;
+
+console.log(mult);
+
+mult--;
+
+console.log(mult);
+
+mult -= 2;
+
+console.log(mult);
+
