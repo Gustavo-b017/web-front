b088517f3e9dc01b0667c700a8912ab9c8ff01a1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:48:02 -0300

    readme e exemplos

---
+++ aulas/aula_07/exercicio casa.js
@@ -0,0 +1,11 @@
+/* 
+- abaixo da constante "cities", declare uma let "upperCaseCities" e atribua á ela um array vazio;
+- Adicione no array que e "upperCaseCities" armazena as ciadades do array cities com todas as letras maiusculas utilizando o for loop;
+- para ver se deu crto, exiba o upperCaseCities no console
+
+toUpperCase();
+*/
+
+const cities = ["Sao paulo", "Rio de janeiro", "minas gerais"]
+
+let upperCaseCities = [];
